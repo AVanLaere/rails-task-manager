0764397523d1d1042431afe0a04eccf1d27402dd
Andy Van Laere <andy@homesweetoregon.com>
2020-04-07 21:49:57 +0300

    cleaner

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
 class TasksController < ApplicationController
-  before_action :set_task, only: [ :show, :edit, :update, :destroy ]
+  before_action :set_task, only: %i[show edit update destroy]
   def index
     @tasks = Task.all
   end
 
-  def show
-  end
+  def show; end
 
   def new
     @task = Task.new
@@ -19,8 +18,7 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
     @task.update(task_params)
